a97ec547b5a6d00062d39d237d8de10a112ec32c
Achourkamal <achourkamal@gmail.com>
2025-05-28 20:05:37 +0100

    Backend- User model

---
+++ src/models/user.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: {
+      validator: (value) => validator.isEmail(value),
+      message: 'Please provide a valid email address'
+    }
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters long']
+  },
+  name: {
+    type: String,
+    required: [true, 'Name is required']
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
+});
+
+// Create unique index on email
+userSchema.index({ email: 1 }, { unique: true });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
